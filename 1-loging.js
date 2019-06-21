b74a90d62002fdedafd2b4db255bee192f5bda43
viruskizz <kizzaraiva@gmail.com>
2019-06-21 16:59:36 +0700

    ver2

--- 1-loging.js
+++ 1-loging.js
@@ -2,7 +2,22 @@
 //  use // for comment สำหรับ คนอ่าน คอมไม่อ่าน
 // /*  */
 
-console.log('555 test');
+console.log('555 test version 2');
 console.log('read text in single quote' + ' use + for connect string');
 console.log('simple calculate: ' + 5 + 10);
 console.log('use , to fix simple calculate: ', (5 + 10));
+/**
+ *  git status
+ *  git pull <romote> <branch>
+ *  git commit
+ *  git add <file> -A
+ *  git push <romote> <branch>
+ *  git checkout
+ *  git checkout -b
+ */
+
+remote(v2) /
+  \
+  stage stage /
+  \
+  local(v1) local(v2)
