6199ecefdd9656548ae7f78aacd2cb46db4f6e98
viruskizz <kizzaraiva@gmail.com>
2019-06-21 15:46:09 +0700

    update log

--- 1-loging.js
+++ 1-loging.js
@@ -2,7 +2,7 @@
 //  use // for comment สำหรับ คนอ่าน คอมไม่อ่าน
 // /*  */
 
-console.log('555');
+console.log('555 test');
 console.log('read text in single quote' + ' use + for connect string');
 console.log('simple calculate: ' + 5 + 10);
 console.log('use , to fix simple calculate: ', (5 + 10));
